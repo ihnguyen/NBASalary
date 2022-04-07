beaca65324b63d635bcf6dc1861a080529512807
ihnguyen <nguyen.thuynhithi@gmail.com>
2022-04-07 16:28:19 -0700

    Added linear model with transformed predictors and omitted player_data

--- NBASalary.R
+++ NBASalary.R
@@ -4,33 +4,14 @@ library(emmeans)
 
 # Read csv files
 salary_data <- read.csv("NBA_season1718_salary.csv")
-player_data <- read.csv("player_data.csv")
 stats_data <- read.csv("Seasons_Stats.csv")
 
-# Omit X column from all data sets
-sal_data <- salary_data %>% 
-  select(-c(X))
-st_data <- stats_data %>% 
-  select(-c(X)) %>% 
-  subset(Year == 2017)
-# Calculate Years of Experience from Player data set
-play_data <- player_data %>% 
-  mutate(YrsExperience = (year_end - year_start))
-
-# Store data sets as data frames and omit irrelevant columns
-st_fdata <- data.frame(st_data) %>% 
-  select(-c(blanl,blank2)); dim(st_fdata)
-sal_fdata <- data.frame(sal_data); dim(sal_fdata)
-play_fdata <- data.frame(play_data) %>% 
-  select(-c(position,year_start,year_end,height,weight)) %>% 
-  rename(Player = name); dim(play_fdata)
-
 # Full Join all data sets
-data0 <- play_fdata %>% 
-  full_join(.,sal_fdata, by="Player") %>% 
+data0 <- sal_fdata %>% 
   full_join(.,st_fdata, by=unique("Player")) %>% 
+  subset(Year == 2017) %>% 
   group_by(Player) %>% 
-  summarise(season17_18 = sum(season17_18), YrsExperience = sum(YrsExperience), Age = mean(Age),
+  summarise(season17_18 = sum(season17_18), Age = mean(Age),
             G = mean(G),GS = mean(GS),MP = mean(MP),PER = mean(PER),TS. = mean(TS.),
             X3PAr =mean(X3PAr),FTr = mean(FTr),ORB. = mean(ORB.),DRB. = mean(DRB.),
             TRB. = mean(TRB.),AST. = mean(AST.),STL. = mean(STL.),BLK. = mean(BLK.),
@@ -39,33 +20,43 @@ data0 <- play_fdata %>%
             FG = mean(FG) ,FGA = mean(FGA) ,FG. = mean(FG.),X3P = mean(X3P) ,X3PA = mean(X3PA) ,
             X2P = mean(X2P) ,X2PA = mean(X2PA) ,X2P. = mean(X2P.) ,eFG. = mean(eFG.),FT = mean(FT),
             FTA = mean(FTA),FT. = mean(FT.),ORB = mean(ORB),DRB = mean(DRB),TRB = mean(TRB),
-            AST = mean(AST),STL = mean(STL),BLK = mean(BLK),TOV = mean(TOV),PF = mean(PF),PTS = mean(PTS)) %>% 
-  na.omit(season17_18); dim(data0)
-# of the 595 observations in the st_fdata, 486 were distinct observations
+            AST = mean(AST),STL = mean(STL),BLK = mean(BLK),TOV = mean(TOV),PF = mean(PF),PTS = mean(PTS)); dim(data0)
+# of the 610 observations in the st_fdata, 486 were distinct observations
 # omitted height & weight since it shortened final data set from 70% to 60%
-403/486
+
 
 # Omit Player column
-final_data <- data0 %>% 
-  select(-Player)
+fdata <- data0 %>% 
+  select(-Player) %>% 
+  na.omit(); dim(fdata)
+403/486
+
+# IF PREDICTOR HAS A ZERO VALUE, WE CAN'T DO LOG TRANSFORMATION
+final_data <- data.frame(fdata)
 
 # Fit the regression
 lm1 <- lm(season17_18 ~ ., data = final_data)
+summary(lm1)
+
+
+# Model with transformed predictors
+lm2 <- lm(season17_18~Age+G+GS+MP+PER+TS.+sqrt(X3PAr)+log(FTr+1)+log(ORB.+1)+DRB.+log(TRB.+1)+log(AST.+1)+STL.+sqrt(BLK.)
+            
+          
+          
+          , data=final_data)
+summary(lm2)
 
 # Check the assumptions
 performance::check_model(lm1)
 
 # Identify predictors that need transformation
-# log transformation
-par(mfrow=c(2,2));hist(log(final_data$YrsExperience),main="Years of Experience");qqPlot(final_data$YrsExperience,main="Years of Experience");plot(final_data$YrsExperience,resid(lm1),data=final_data);plot(log(final_data$YrsExperience),resid(lm1),data=final_data)
-
 # sqrt transformation? doesn't look much different
 par(mfrow=c(2,2));hist(final_data$Age, main="Age");qqPlot(final_data$Age);plot(final_data$Age,resid(lm1),data=final_data);plot(sqrt(final_data$Age),resid(lm1),data=final_data)
 
 # unsure which tx to do for Games
 par(mfrow=c(2,2));hist(final_data$G, main="Games");qqPlot(final_data$G);plot(final_data$G,resid(lm1),data=final_data)
 
-# log transformation; possibly better?
 par(mfrow=c(2,2));hist(final_data$GS, main="Games Started");qqPlot(final_data$GS);plot((final_data$GS),resid(lm1),data=final_data);plot(log(final_data$GS),resid(lm1),data=final_data)
 
 par(mfrow=c(2,2));hist(final_data$MP, main="Minutes Played");qqPlot(final_data$MP);plot(final_data$MP,resid(lm1),data=final_data)
@@ -96,12 +87,7 @@ par(mfrow=c(2,2));hist(final_data$STL., main="Steal Percentage");qqPlot(final_da
 # log transformation; either sqrt and log work here
 par(mfrow=c(2,2));hist(final_data$BLK., main="Block Percentage");qqPlot(final_data$BLK.);plot((final_data$BLK.),resid(lm1),data=final_data);plot(log(final_data$BLK.),resid(lm1),data=final_data)
 
-# final_data[,1] <- sapply(final_data[,1], as.factor)
-# str(final_data)
-# final_data <- data.frame(final_data)
-# # Model with transformed predictors
-# lm2 <- lm(season17_18~log(YrsExperience)+Age+G+GS+MP+PER+TS.+sqrt(X3PAr)+log(FTr)+log(ORB.)+DRB.+log(TRB.)+log(AST.)+STL.+log(BLK.),
-#           data=final_data)
+
 
 # Correlation matrix as a data frame
 corr_df <- round(cor(final_data[,1:47]),2) #Not including response variable, Salary, which is 50th column
