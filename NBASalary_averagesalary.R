7e0cc9decf832edf5224f81686b4690f07edf077
ihnguyen <nguyen.thuynhithi@gmail.com>
2022-04-24 15:09:03 -0700

    Updated USG. to log tx and added new mokdels: lm4-lm4i

--- NBASalary_averagesalary.R
+++ NBASalary_averagesalary.R
@@ -79,7 +79,7 @@ round(cor(final_data[,c(1:46)]),4)
 lm1 <- lm(season17_18 ~ ., data = final_data); summary(lm1);performance::check_model(lm1)
 # Fit regression with transformed predictors and response (Full model)
 lm2 <- lm((season17_18)^0.33~Age+G+GS+MP+PER+TS.+sqrt(X3PAr)+log(FTr+4)+log(ORB.+1)+DRB.+log(TRB.+1)+log(AST.+1)+STL.+sqrt(BLK.)
-          + log(TOV.+ 20) + USG. + log(OWS+ 20) + sqrt(DWS+ 20) + sqrt(WS+ 20) + WS.48 + sqrt(OBPM + 20) + sqrt(DBPM + 20) + BPM + log(VORP+ 20) + sqrt(FG+ 20) + FG.+ sqrt(FGA) + sqrt(X3PA+ 20) + sqrt(X2P+ 20) + 
+          + log(TOV.+ 20) + log(USG.) + log(OWS+ 20) + sqrt(DWS+ 20) + sqrt(WS+ 20) + WS.48 + sqrt(OBPM + 20) + sqrt(DBPM + 20) + BPM + log(VORP+ 20) + sqrt(FG+ 20) + FG.+ sqrt(FGA) + sqrt(X3PA+ 20) + sqrt(X2P+ 20) + 
             sqrt(X2PA+ 20) + log(X2P.+ 20) + log(eFG.+ 20) + log(FT+ 20) + log(FTA +6) + FT. + log(ORB+ 20) + sqrt(DRB+ 20) + sqrt(TRB+ 20) + log(AST+ 20) + sqrt(STL+ 20) + log(BLK+ 20) + sqrt(TOV+ 20) + PF + sqrt(PTS+ 20)
           , data=final_data); summary(lm2); performance::check_model(lm2)
 # Check if response needs to be transformed using Box-Cox transformation
@@ -95,13 +95,19 @@ n = nrow(final_data)
 lm3 <- update(lm2, ~. - log(eFG. + 20) - log(TRB. + 1) - log(OWS + 20) - sqrt(FG + 20) - sqrt(X2P + 20) - sqrt(X2PA + 20) - log(FT + 20) - 
                 sqrt(TRB + 20) - sqrt(X3PA + 20) - sqrt(FGA)); summary(lm3); performance::check_model(lm3)
 # Drop rejects (rho>0.83) with high collinearity & lower correlation with the response variable
-lm4 <- update(lm3,~. -log(ORB+20)-log(FT+20)-sqrt(X2PA+20)-sqrt(TOV+20)-sqrt(X2P+20)-log(X2P.+20))
-lm4a <- step(lm4, k = log(n));summary(lm4a); performance::check_model(lm4a);
+lm4 <- update(lm3,~. -log(ORB+20)-log(FT+20)-sqrt(X2PA+20)-sqrt(TOV+20)-sqrt(X2P+20)-log(X2P.+20));summary(lm4); performance::check_model(lm4)
+lm4a <- step(lm4);summary(lm4a); performance::check_model(lm4a);
 anova(lm4,lm2);anova(lm4a,lm2)
-lm4b <- update(lm4a,~.-sqrt(X3PAr)-log(VORP+20));summary(lm4b); performance::check_model(lm4b);anova(lm4b,lm2)
-lm4c <- update(lm4b,~.-BPM -PER);summary(lm4c); performance::check_model(lm4c);anova(lm4c,lm2) #################### good model
-lm4d <- update(lm4c,~.-USG.);summary(lm4d); performance::check_model(lm4d);anova(lm4d,lm2)
-lm4e <- update(lm4c,~.-G);summary(lm4e); performance::check_model(lm4e);anova(lm4e,lm2)
+lm4b <- update(lm4a,~.-PF);summary(lm4b); performance::check_model(lm4b);anova(lm4b,lm2)
+lm4c <- update(lm4b,~.-sqrt(BLK.));summary(lm4c); performance::check_model(lm4c);anova(lm4c,lm2) #################### good model
+lm4d <- update(lm4c,~.-GS);summary(lm4d); performance::check_model(lm4d);anova(lm4d,lm2)
+lm4e <- update(lm4d,~.-sqrt(DBPM + 20));summary(lm4e); performance::check_model(lm4e);anova(lm4e,lm2)
+lm4f <- update(lm4e,~.-DRB.);summary(lm4f); performance::check_model(lm4f);anova(lm4f,lm2)
+lm4g <- update(lm4f,~.-BPM);summary(lm4g); performance::check_model(lm4g);anova(lm4g,lm2)
+lm4h <- update(lm4g,~.-PER);summary(lm4h); performance::check_model(lm4h);anova(lm4h,lm2)
+
+lm4i <- step(lm4, k=log(n)); summary(lm4i); performance::check_model(lm4i)
+
 #lm4 <- update(lm3, ~. - G - MP - WS.48 - log(ORB. + 1) - sqrt(TRB + 20) - log(ORB + 20) - log(VORP + 20) - log(FTA + 6) - 
 #                sqrt(OBPM + 20) - log(FT + 20) - sqrt(TOV + 20) - log(X2P. + 20) - FG. - sqrt(X2P + 20) - sqrt(X2PA + 20) -
 #                log(AST + 20)); summary(lm4); performance::check_model(lm4)
@@ -153,6 +159,11 @@ anova(lm4a,lm2) # keep lm4a
 anova(lm4b,lm2) # keep lm4b
 anova(lm4c,lm2) # keep lm4c
 anova(lm4d,lm2) # its okay
+anova(lm4e,lm2) # its okay
+anova(lm4f,lm2) # its okay
+anova(lm4g,lm2) # its okay
+anova(lm4h,lm2) # its okay
+anova(lm4i, lm2) # its okay
 
 anova(lm19,lm2) # keep lm19
 anova(lm19a,lm2) # keep lm19a
@@ -180,6 +191,11 @@ s4b <- summary(lm4b)$adj.r.squared
 s4c <- summary(lm4c)$adj.r.squared
 s4d <- summary(lm4d)$adj.r.squared
 s4e <- summary(lm4e)$adj.r.squared
+s4f <- summary(lm4f)$adj.r.squared
+s4g <- summary(lm4g)$adj.r.squared
+s4h <- summary(lm4h)$adj.r.squared
+s4i <- summary(lm4i)$adj.r.squared
+
 s19 <- summary(lm19)$adj.r.squared
 s19a <- summary(lm19a)$adj.r.squared
 s19b <- summary(lm19b)$adj.r.squared
@@ -200,31 +216,30 @@ s22g <- summary(lm22g)$adj.r.squared
 s22h <- summary(lm22h)$adj.r.squared
 s23 <- summary(lm23)$adj.r.squared
 # View adjusted R squared
-s2;s4;s4a;s4b;s4c;s4d;s4e
+s2;s4;s4a;s4b;s4c;s4d;s4e;s4f;s4g;s4h;s4i
 s2;s2a;s2b;s2c;s3;s3a;s3b;s3c;s19;s19a;s19b;s19c;s19d;s21;s21a;s21b;s21c;s22;s22a;s22b;s22c;s22d;s22e;s22f;s22g;s22h;s23
 BIC(lm2,lm2a,lm2b,lm2c,lm3,lm3a,lm3b,lm3c,lm19,lm19a,lm19b,lm19c,lm19d,lm21,lm21a,lm21b,lm21c,lm22,lm22a,lm22b,lm22c,lm22d,lm22e,lm22f,lm22g,lm22h,lm23)
-BIC(lm2,lm4,lm4a,lm4b,lm4c,lm4d,lm4e)
+AIC(lm2,lm4,lm4a,lm4b,lm4c,lm4d,lm4e,lm4f,lm4g,lm4h)
+BIC(lm2,lm4i,lm19)
 #lm21,lm22,lm22c,lm22g all have high collinearity
 #lm22d and lm21a have high collinearity
 #lm22a has the next best BIC 
 round(cor(final_data[,c(1,2,3,4,19,46)]),4)
-pairs((season17_18)^0.33 ~ Age + G + MP + DRB. + USG. + 
-        sqrt(WS + 20), data = final_data)
+pairs((season17_18)^0.33 ~ Age + G + sqrt(DWS+20) + sqrt(PTS+20), data = final_data)
 ################################################ CROSS VALIDATION ##################################################################
 set.seed(111)
 n <- nrow(final_data);n
 z <- floor(0.7*n)
 train <- sample(1:n, z)
 test_data <- final_data[-train,]
-# Train models full model lm2 (lm_train1) and lm4c (lm_train2)
+# Train models full model lm2 (lm_train1) and lm4i (lm_train2)
 lm_train1 <- lm(season17_18^0.33 ~ Age + G + GS + MP + PER + TS. + 
                   sqrt(X3PAr) + log(FTr + 4) + log(ORB. + 1) + DRB. + log(TRB. + 1) + log(AST. + 1) + STL. + sqrt(BLK.) + log(TOV. + 20) + 
                   USG. + log(OWS + 20) + sqrt(DWS + 20) + sqrt(WS + 20) + WS.48 + sqrt(OBPM + 20) + sqrt(DBPM + 20) + BPM + log(VORP + 20) + 
                   sqrt(FG + 20) + FG. + sqrt(FGA) + sqrt(X3PA + 20) + sqrt(X2P + 20) + sqrt(X2PA + 20) + log(X2P. + 20) + log(eFG. + 20) + 
                   log(FT + 20) + log(FTA + 6) + FT. + log(ORB + 20) + sqrt(DRB + 20) + sqrt(TRB + 20) + log(AST + 20) + sqrt(STL + 20) +
                   log(BLK + 20) + sqrt(TOV + 20) + PF + sqrt(PTS + 20), data = final_data, subset=train); summary(lm_train1)
-lm_train2 <- lm((season17_18)^0.33 ~ Age + G + MP + DRB. + USG. + 
-                  sqrt(WS + 20), data = final_data, subset=train);summary(lm_train2)
+lm_train2 <- lm((season17_18)^0.33 ~ Age + G + sqrt(DWS+20) + sqrt(PTS+20), data = final_data, subset=train);summary(lm_train2)
 # Calculate R2, RMSE, and MAE
 predictions1 <- lm_train1 %>% predict(test_data)
 data.frame(R2 = R2(predictions1, test_data$season17_18^0.33),
@@ -236,45 +251,47 @@ data.frame(R2 = R2(predictions2, test_data$season17_18^0.33),
            MAE = MAE(predictions2, test_data$season17_18^0.33))
 ##################################### VERIFY AND CALCULATE PREDICTION INTERVAL ##################################################################
 # Test predictability on 2017-18 salary
-pred_dame1 <- predict(lm4c,newdata = data.frame(Age=26,G=75.000000,MP = 2694.00000,DRB. = 13.300000,USG. = 31.500000 , WS=10.30000000), interval="prediction")
+pred_dame1 <- predict(lm4i,newdata = data.frame(Age=26,G=75.000000,DWS=1.50000000,PTS=2024.000000), interval="prediction")
 pred_dame1^3
-# fit          lwr          upr
-# 18,273,322   6,152,846    40,546,556
+# fit      lwr      upr
+# 14,481,628 4,331,569 34,164,723
 # Actual 2017-18 Salary = 26,153,057.0
 
 # Test predictability on 2020-21 salary
 # https://www.basketball-reference.com/leagues/NBA_2021_advanced.html
-pred_dame2 <- predict(lm4c,newdata = data.frame(Age=30,G=67,MP = 2398,DRB. = 11.5,USG. = 31.4 , WS=10.4), interval="prediction")
+pred_dame2 <- predict(lm4i,newdata = data.frame(Age=30,G=67,DWS=0.8,PTS=1928), interval="prediction")
 pred_dame2^3
-# fit          lwr          upr
-# 17,863,938   5,929,018    39,942,632
+# fit        lwr       upr
+# 13,935,753 4,048,273 33,359,456
 # Actual 2021-22 Salary = 31,626,953
 # Note: 2020-21 salary was calculated rather than 2021-22 salary due to Lillard's injury which doesn't give an accurate estimated salary
 ############################################################## FIGURES AND TABLES ###########################################################################
 # Observe data with statistically significant predictors (lm4c) using Box Plot
 dd <- final_data %>% 
-  dplyr::rename(Games = G) %>% 
-  dplyr::rename(MinutesPlayed = MP) %>% 
-  dplyr::rename(DefensiveReboundPercentage = DRB.) %>% 
-  dplyr::rename(UsagePercentage = USG.) %>% 
-  dplyr::rename(WinShares = WS) %>%
+  dplyr::rename("Games" = G) %>% 
+  dplyr::rename("Defensive Win Shares" = DWS) %>% 
+  dplyr::rename("Points" = PTS) %>%
   dplyr::mutate(Salary = season17_18)
 d <- melt(dd, id="season17_18")
 ggplot(d,aes(x=variable,y=value,color=variable)) +
   geom_boxplot() +
   theme(axis.text.x = element_text(size=10, angle=45))
-dt <- dd[,c(1,2,3,5,11,17,20,47)]
-dt1 <- dd[,c(1,2,3,11,17,20)]
-dt2 <- dd[,c(1,5)]
-dt3 <- dd[,c(1,47)]
+dt <- dd[,c(1,2,3,19,46)]
+dt1 <- dd[,c(1,2,3)]
+dt2 <- dd[,c(1,19)]
+dt3 <- dd[,c(1,46)]
+dt4 <- dd[,c(1,47)]
 e <- melt(dt,id="season17_18")
 e1 <- melt(dt1,id="season17_18")
 e2 <- melt(dt2,id="season17_18")
 e3 <- melt(dt3,id="season17_18")
+e4 <- melt(dt4,id="season17_18")
+
 ggplot(e,aes(x=variable,y=value)) + geom_boxplot() + xlab("") + ylab("")
 ggplot(e1,aes(x=variable,y=value)) + geom_boxplot(fill='#FFF2CC', color="black") + xlab("") + ylab("") + theme(axis.text.x = element_text(size=8, angle=45))
 ggplot(e2,aes(x=variable,y=value)) + geom_boxplot(fill='#FFF2CC', color="black") + xlab("") + ylab("")+ theme(text = element_text(size = 12))
-ggplot(e3,aes(x=variable,y=value)) + geom_boxplot(fill='#B6D7A8', color="black") + scale_y_continuous(labels = label_number(suffix = " M", scale = 1e-6)) + xlab("") + ylab("")+ theme(text = element_text(size = 12))
+ggplot(e3,aes(x=variable,y=value)) + geom_boxplot(fill='#FFF2CC', color="black") + xlab("") + ylab("")+ theme(text = element_text(size = 12))
+ggplot(e4,aes(x=variable,y=value)) + geom_boxplot(fill='#B6D7A8', color="black") + scale_y_continuous(labels = label_number(suffix = " M", scale = 1e-6)) + xlab("") + ylab("")+ theme(text = element_text(size = 12))
 # Boxplots without color for research paper
 layout(matrix(c(1,2,3),1,3,byrow=T))
 ggplot(e1,aes(x=variable,y=value)) + geom_boxplot() + xlab("") + ylab("") + theme(axis.text.x = element_text(size=8, angle=45))
