475927f6840a671e504a24da31a4179f29f47422
ihnguyen <nguyen.thuynhithi@gmail.com>
2022-04-27 12:38:29 -0700

    Cleaned up code for final paper

--- NBASalary_averagesalary.R
+++ NBASalary_averagesalary.R
@@ -77,106 +77,64 @@ round(cor(final_data[,c(1:46)]),4)
 ##################################### OVERALL F-TEST AND MODELING ##################################################################
 # Fit the regression, summarize and check assumptions
 lm1 <- lm(season17_18 ~ ., data = final_data); summary(lm1);performance::check_model(lm1)
+# Check if response needs to be transformed using Box-Cox transformation
+ptf<- powerTransform(season17_18~.,data = final_data)
+summary(ptf)
 # Fit regression with transformed predictors and response (Full model)
 lm2 <- lm((season17_18)^0.33~Age+G+GS+MP+PER+TS.+sqrt(X3PAr)+log(FTr+4)+log(ORB.+1)+DRB.+log(TRB.+1)+log(AST.+1)+STL.+sqrt(BLK.)
           + log(TOV.+ 20) + log(USG.) + log(OWS+ 20) + sqrt(DWS+ 20) + sqrt(WS+ 20) + WS.48 + sqrt(OBPM + 20) + sqrt(DBPM + 20) + BPM + log(VORP+ 20) + sqrt(FG+ 20) + FG.+ sqrt(FGA) + sqrt(X3PA+ 20) + sqrt(X2P+ 20) + 
             sqrt(X2PA+ 20) + log(X2P.+ 20) + log(eFG.+ 20) + log(FT+ 20) + log(FTA +6) + FT. + log(ORB+ 20) + sqrt(DRB+ 20) + sqrt(TRB+ 20) + log(AST+ 20) + sqrt(STL+ 20) + log(BLK+ 20) + sqrt(TOV+ 20) + PF + sqrt(PTS+ 20)
           , data=final_data); summary(lm2); performance::check_model(lm2)
-# Check if response needs to be transformed using Box-Cox transformation
-ptf<- powerTransform(season17_18~.,data = final_data)
-summary(ptf)
-# Visual Diagnositics
+# Visual Diagnostics of untransformed and transformed response
 par(mfrow=c(1,2));qqPlot(final_data$season17_18, xlab="Salary",ylab="Residuals");qqPlot((final_data$season17_18)^0.33, xlab="Salary^0.33",ylab="Residuals")
 par(mfrow=c(1,2));hist(final_data$season17_18, main="", xlab="Salary");hist((final_data$season17_18)^0.33,main="", xlab="Salary^0.33")
 par(mfrow=c(1,2));plot(final_data$season17_18,resid(lm2),data=final_data, xlab="Salary",ylab="Residuals");plot((final_data$season17_18)^0.33,resid(lm2),data=final_data, xlab="Salary^0.33",ylab="Residuals")
 # Is there a relationship between the response and at least one predictor in our regression model?
 null <- lm((season17_18)^0.33~1, data=final_data)
 anova(null,lm2)
-############################### BACKWARDS ELIMINATION (VARIABLE SELECTION) ############################################################
+############################### BACKWARDS ELIMINATION & STEPWISE SELECTION (VARIABLE SELECTION) ############################################################
 n = nrow(final_data)
 # Drop rejects (rho>0.9) with high collinearity & lower correlation with the response variable
 lm3 <- update(lm2, ~. - log(eFG. + 20) - log(TRB. + 1) - log(OWS + 20) - sqrt(FG + 20) - sqrt(X2P + 20) - sqrt(X2PA + 20) - log(FT + 20) - 
                 sqrt(TRB + 20) - sqrt(X3PA + 20) - sqrt(FGA)); summary(lm3); performance::check_model(lm3)
 # Drop rejects (rho>0.83) with high collinearity & lower correlation with the response variable
-lm4 <- update(lm3,~. -log(ORB+20)-log(FT+20)-sqrt(X2PA+20)-sqrt(TOV+20)-sqrt(X2P+20)-log(X2P.+20));summary(lm4); performance::check_model(lm4)
-lm4a <- step(lm4);summary(lm4a); performance::check_model(lm4a);
-anova(lm4,lm2);anova(lm4a,lm2)
-lm4b <- update(lm4a,~.-PF);summary(lm4b); performance::check_model(lm4b);anova(lm4b,lm2)
-lm4c <- update(lm4b,~.-sqrt(BLK.));summary(lm4c); performance::check_model(lm4c);anova(lm4c,lm2) #################### good model
-lm4d <- update(lm4c,~.-GS);summary(lm4d); performance::check_model(lm4d);anova(lm4d,lm2)
-lm4e <- update(lm4d,~.-sqrt(DBPM + 20));summary(lm4e); performance::check_model(lm4e);anova(lm4e,lm2)
-lm4f <- update(lm4e,~.-DRB.);summary(lm4f); performance::check_model(lm4f);anova(lm4f,lm2)
-lm4g <- update(lm4f,~.-BPM);summary(lm4g); performance::check_model(lm4g);anova(lm4g,lm2)
-lm4h <- update(lm4g,~.-PER);summary(lm4h); performance::check_model(lm4h);anova(lm4h,lm2)
+lm4 <- update(lm3,~. -log(ORB+20)-log(FT+20)-sqrt(X2PA+20)-sqrt(TOV+20)-sqrt(X2P+20)-log(X2P.+20));summary(lm4); performance::check_model(lm4);anova(lm4,lm2)
+# Run AIC model after dropping rejects with rho>0.83 and perform backwards elimination
+lm4a <- step(lm4);summary(lm4a); performance::check_model(lm4a);anova(lm4a,lm2)
+  lm4b <- update(lm4a,~.-PF);summary(lm4b); performance::check_model(lm4b);anova(lm4b,lm2)
+  lm4c <- update(lm4b,~.-sqrt(BLK.));summary(lm4c); performance::check_model(lm4c);anova(lm4c,lm2) #################### good model
+  lm4d <- update(lm4c,~.-GS);summary(lm4d); performance::check_model(lm4d);anova(lm4d,lm2)
+  lm4e <- update(lm4d,~.-sqrt(DBPM + 20));summary(lm4e); performance::check_model(lm4e);anova(lm4e,lm2)
+  lm4f <- update(lm4e,~.-DRB.);summary(lm4f); performance::check_model(lm4f);anova(lm4f,lm2)
+  lm4g <- update(lm4f,~.-BPM);summary(lm4g); performance::check_model(lm4g);anova(lm4g,lm2)
+  lm4h <- update(lm4g,~.-PER);summary(lm4h); performance::check_model(lm4h);anova(lm4h,lm2)
+# Run BIC model after dropping rejects with rho>0.83
 lm4i <- step(lm4, k=log(n)); summary(lm4i); performance::check_model(lm4i)
-plot(final_data$G, final_data$MP,data=final_data)
-################################ BACKWARDS STEPWISE SELECTION & ELIMINATION (VARIABLE SELECTION) ######################################
-lm19 <- step(lm3, k = log(n));summary(lm19); performance::check_model(lm19)
-  lm19a <- update(lm19,  ~ . - sqrt(X3PAr)); summary(lm19a);performance::check_model(lm19a)
-  lm19b <- update(lm19a,  ~ . - PER); summary(lm19b);performance::check_model(lm19b)
-  lm19c <- update(lm19b,  ~ . - log(VORP + 20)); summary(lm19c);performance::check_model(lm19c)
-  lm19d <- update(lm19c,  ~ . - BPM); summary(lm19d);performance::check_model(lm19d)
+# Run BIC model without dropping rejects with high collinearity & lower correlation with response variable and perform backwards elimination
 lm21 <- step(lm2, k = log(n));summary(lm21); performance::check_model(lm21)
   lm21a <- update(lm21,~.-GS);summary(lm21a); performance::check_model(lm21a)
   lm21b <- update(lm21a,~.-log(OWS+20));summary(lm21b); performance::check_model(lm21b)
-  lm21c <- update(lm21b,~.-sqrt(X2P+20));summary(lm21c); performance::check_model(lm21c) ## good?
-lm24 <- step(lm4, k=log(n));summary(lm24); performance::check_model(lm24)
-# lm22 <- step(lm2a, k = log(n));summary(lm22); performance::check_model(lm22)
-#   lm22a <- step(lm3a, k = log(n));summary(lm22a); performance::check_model(lm22a)
-#   lm22b <- step(lm3b, k = log(n));summary(lm22b); performance::check_model(lm22b)
-#   lm22c <- step(lm2b, k = log(n));summary(lm22c); performance::check_model(lm22c)
-#   lm22d <- update(lm22c,~. -GS); summary(lm22d);performance::check_model(lm22d)
-#   lm22e <- update(lm22d,~. -sqrt(X2P+20)); summary(lm22e);performance::check_model(lm22e)
-#   lm22f <- update(lm22e,~. -sqrt(X3PAr)); summary(lm22f);performance::check_model(lm22f)
-#   lm22g <- step(lm2a, k = log(n));summary(lm22g);performance::check_model(lm22g)
-#   lm22h <- update(lm22a,~.-G);summary(lm22h);performance::check_model(lm22h)
-# lm23 <- step(lm2c, k =log(n));summary(lm23);performance::check_model(lm23)
-# View scatterplot matrices
-pairs((season17_18)^0.33 ~ Age + G + sqrt(X3PAr) + log(OWS + 20) + sqrt(DWS + 20) + sqrt(FGA), data = final_data)
-round(cor(final_data[,c(1,2,3,8,18,19,27)]),4)
-pairs((season17_18)^0.33 ~ Age + G + log(OWS + 20) + sqrt(DWS + 20) + sqrt(FGA), data = final_data)
-round(cor(final_data[,c(1,2,3,18,19,27)]),4)
+  lm21c <- update(lm21b,~.-sqrt(X2P+20));summary(lm21c); performance::check_model(lm21c)
 ################################################ MODEL SELECTION ##################################################################
 # Compare all models against full model
-anova(lm2a,lm2) # keep lm2a
-anova(lm2b,lm2) # keep lm2b
-anova(lm2c,lm2) # use lm2
 anova(lm3,lm2) # keep lm3
-anova(lm3a,lm2) # keep lm3a
-anova(lm3b,lm2) # keep lm3b
-anova(lm3c,lm2) # use lm2
-
 anova(lm4,lm2) # keep lm4
 anova(lm4a,lm2) # keep lm4a
 anova(lm4b,lm2) # keep lm4b
 anova(lm4c,lm2) # keep lm4c
-anova(lm4d,lm2) # its okay
-anova(lm4e,lm2) # its okay
-anova(lm4f,lm2) # its okay
-anova(lm4g,lm2) # its okay
-anova(lm4h,lm2) # its okay
-anova(lm4i, lm2) # its okay
-
-anova(lm19,lm2) # keep lm19
-anova(lm19a,lm2) # keep lm19a
-anova(lm19b,lm2) # keep lm19b
-anova(lm19c,lm2) # keep lm19c
-anova(lm19d,lm2) # keep lm19d
+anova(lm4d,lm2) # keep lm4d
+anova(lm4e,lm2) # keep lm4e
+anova(lm4f,lm2) # keep lm4f
+anova(lm4g,lm2) # keep lm4g
+anova(lm4h,lm2) # keep lm4h
+anova(lm4i, lm2) # keep lm4i
 anova(lm21,lm2) # keep lm21
 anova(lm21a,lm2)# keep lm21a
 anova(lm21b,lm2) # keep lm21b
 anova(lm21c,lm2) # use lm2
-anova(lm24,lm2)
-
 # Assign models to a name
 s2 <- summary(lm2)$adj.r.squared
-s2a <- summary(lm2a)$adj.r.squared
-s2b <- summary(lm2b)$adj.r.squared
-s2c <- summary(lm2c)$adj.r.squared
 s3 <- summary(lm3)$adj.r.squared
-s3a <- summary(lm3a)$adj.r.squared
-s3b <- summary(lm3b)$adj.r.squared
-s3c <- summary(lm3c)$adj.r.squared
 s4 <- summary(lm4)$adj.r.squared
 s4a <- summary(lm4a)$adj.r.squared
 s4b <- summary(lm4b)$adj.r.squared
@@ -187,36 +145,19 @@ s4f <- summary(lm4f)$adj.r.squared
 s4g <- summary(lm4g)$adj.r.squared
 s4h <- summary(lm4h)$adj.r.squared
 s4i <- summary(lm4i)$adj.r.squared
-
-s19 <- summary(lm19)$adj.r.squared
-s19a <- summary(lm19a)$adj.r.squared
-s19b <- summary(lm19b)$adj.r.squared
-s19c <- summary(lm19c)$adj.r.squared
-s19d <- summary(lm19d)$adj.r.squared
 s21 <- summary(lm21)$adj.r.squared
 s21a <- summary(lm21a)$adj.r.squared
 s21b <- summary(lm21b)$adj.r.squared
 s21c <- summary(lm21c)$adj.r.squared
-s22 <- summary(lm22)$adj.r.squared
-s22a <- summary(lm22a)$adj.r.squared
-s22b <- summary(lm22b)$adj.r.squared
-s22c <- summary(lm22c)$adj.r.squared
-s22d <- summary(lm22d)$adj.r.squared
-s22e <- summary(lm22e)$adj.r.squared
-s22f <- summary(lm22f)$adj.r.squared
-s22g <- summary(lm22g)$adj.r.squared
-s22h <- summary(lm22h)$adj.r.squared
-s23 <- summary(lm23)$adj.r.squared
 # View adjusted R squared
 s2;s4;s4a;s4b;s4c;s4d;s4e;s4f;s4g;s4h;s4i
-s2;s2a;s2b;s2c;s3;s3a;s3b;s3c;s19;s19a;s19b;s19c;s19d;s21;s21a;s21b;s21c;s22;s22a;s22b;s22c;s22d;s22e;s22f;s22g;s22h;s23
-BIC(lm2,lm2a,lm2b,lm2c,lm3,lm3a,lm3b,lm3c,lm19,lm19a,lm19b,lm19c,lm19d,lm21,lm21a,lm21b,lm21c,lm22,lm22a,lm22b,lm22c,lm22d,lm22e,lm22f,lm22g,lm22h,lm23)
+s2;s3;s21;s21a;s21b;s21c
+# Run BIC/AIC model selection to determine model with lower AIC or BIC
+BIC(lm2,lm3,lm21,lm21a,lm21b,lm21c)
 AIC(lm2,lm4,lm4a,lm4b,lm4c,lm4d,lm4e,lm4f,lm4g,lm4h)
-BIC(lm2,lm4i,lm19)
-#lm21,lm22,lm22c,lm22g all have high collinearity
-#lm22d and lm21a have high collinearity
-#lm22a has the next best BIC 
-round(cor(final_data[,c(1,2,3,4,19,46)]),4)
+BIC(lm2,lm4i)
+# Create correlation and scatter plot matrices of best model (lower BIC than full model, no multicollinearity, significant predictors, and met all assumptions reasonably)
+round(cor(final_data[,c(1,2,3,19,46)]),4)
 pairs((season17_18)^0.33 ~ Age + G + sqrt(DWS+20) + sqrt(PTS+20), data = final_data)
 ################################################ CROSS VALIDATION ##################################################################
 set.seed(111)
@@ -258,22 +199,17 @@ pred_dame2^3
 # Actual 2021-22 Salary = 31,626,953
 # Note: 2020-21 salary was calculated rather than 2021-22 salary due to Lillard's injury which doesn't give an accurate estimated salary
 ############################################################## FIGURES  ###########################################################################
-# Observe data with statistically significant predictors (lm4i) using Box Plot
+# Observe data with statistically significant predictors (lm4i) using Box Plot for paper and presentation
 dd <- final_data %>% 
   dplyr::rename("Games" = G) %>% 
   dplyr::rename("Defensive Win Shares" = DWS) %>% 
   dplyr::rename("Points" = PTS) %>%
   dplyr::mutate(Salary = season17_18)
 d <- melt(dd, id="season17_18")
-ggplot(d,aes(x=variable,y=value,color=variable)) +
-  geom_boxplot() +
-  theme(axis.text.x = element_text(size=10, angle=45))
-dt <- dd[,c(1,2,3,19,46)]; e <- melt(dt,id="season17_18")
 dt1 <- dd[,c(1,2,3)]; e1 <- melt(dt1,id="season17_18")
 dt2 <- dd[,c(1,19)]; e2 <- melt(dt2,id="season17_18")
 dt3 <- dd[,c(1,46)]; e3 <- melt(dt3,id="season17_18")
 dt4 <- dd[,c(1,47)]; e4 <- melt(dt4,id="season17_18")
-ggplot(e,aes(x=variable,y=value)) + geom_boxplot() + xlab("") + ylab("")
 ggplot(e1,aes(x=variable,y=value)) + geom_boxplot(fill='#FFF2CC', color="black") + xlab("") + ylab("")+ theme(text = element_text(size = 12))# + theme(axis.text.x = element_text(size=8, angle=45))
 ggplot(e2,aes(x=variable,y=value)) + geom_boxplot(fill='#FFF2CC', color="black") + xlab("") + ylab("")+ theme(text = element_text(size = 12))
 ggplot(e3,aes(x=variable,y=value)) + geom_boxplot(fill='#FFF2CC', color="black") + xlab("") + ylab("")+ theme(text = element_text(size = 12))
@@ -283,20 +219,20 @@ ggplot(e1,aes(x=variable,y=value)) + geom_boxplot() + xlab("") + ylab("")+ theme
 ggplot(e2,aes(x=variable,y=value)) + geom_boxplot() + xlab("") + ylab("")+ theme(text = element_text(size = 12))
 ggplot(e3,aes(x=variable,y=value)) + geom_boxplot() + xlab("") + ylab("")+ theme(text = element_text(size = 12))
 ggplot(e4,aes(x=variable,y=value)) + geom_boxplot() + scale_y_continuous(labels = label_number(suffix = " M", scale = 1e-6)) + xlab("") + ylab("")+ theme(text = element_text(size = 12))
-#Scatterplot matrix untransformed
+# Scatterplot matrix untransformed for presentation purposes
 pairs(season17_18~Age+G+DWS+PTS,data=final_data)
 ############################################################## TABLES ###########################################################################
-# Cross-Validation Tables
+# Cross-Validation Tables for paper and presentation
 aa %>% kbl() %>% kable_classic(full_width=F,html_font="Arial")
 ab %>% kbl() %>% kable_classic(full_width=F,html_font="Arial")
-# Summary Table with gt package
+# Summary Table with gt package for paper and presentation
 t1 <- tbl_regression(lm4i);t1
-# Summary Table with knitr::kableExtra
+# Summary Table with knitr::kableExtra for paper purposes
 knit <- coef(summary(lm4i))
 knit %>% 
   kbl() %>% 
   kable_classic(full_width=F,html_font="Arial")
-# Descriptive Statistics with gt package (may not use since similar to boxplots)
+# Descriptive Statistics with gt package (Optional use due to similarity to box plots)
 list <- final_data[1:12] %>% 
   tbl_summary(
     statistic = ~"{mean}; {median} ({min},{max})"); list
